a0de354c42a7fc26d3d5f79936b8e188c6707e95
Moritz Eich <hey@moritz.dev>
2021-11-08 19:04:27 +0100

    add: basic view for home page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Home} />
+        <Route path="/" element={<Home />} />
         <Route path=":shortUrl" element={Redirect} />
       </Routes>
     </BrowserRouter>
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,3 +1,35 @@
-const Home = null;
+import "../style.css";
+import { useState } from "react";
+
+const Home = () => {
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    /* TODO
+        - create short url
+        - update stats
+    */
+  };
+
+  /* TODO
+    - styling + fancy animation
+    - stats
+  */
+
+  return (
+    <div className="home_container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
