deb9831d19d7f760cdf40c81c8fcc69943d1fa70
Moritz Eich <hey@moritz.dev>
2021-11-10 16:40:46 +0100

    add: url validation

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,15 +2,22 @@ import "../style.css";
 import { useState } from "react";
 
 import { checkIfUrlExists, createShortUrl, getShortUrl } from "../api";
+import { validateUrl } from "../util";
 
 const Home = () => {
   const [url, setUrl] = useState("");
-  const [shortUrl, setShortUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     let shortUrl = "";
+
+    if (!validateUrl(url)) {
+      setError("Url is invalid.");
+      return;
+    }
 
     if (await checkIfUrlExists(url)) {
       shortUrl = await getShortUrl(url);
@@ -18,12 +25,14 @@ const Home = () => {
       shortUrl = await createShortUrl(url);
     }
 
+    setError(null);
     setShortUrl(shortUrl);
   };
 
   const handleReset = () => {
     setUrl("");
     setShortUrl("");
+    setError("");
   };
 
   /* TODO
@@ -45,6 +54,7 @@ const Home = () => {
       </div>
 
       {shortUrl && <div>{shortUrl}</div>}
+      {error && <p>{error}</p>}
     </div>
   );
 };
--- src/util.js
+++ src/util.js
@@ -4,3 +4,16 @@ export const encodeUrl = (id) => {
   const encoding = base62.encode(id);
   return encoding;
 };
+
+export const validateUrl = (urlString) => {
+  // Taken from: https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+  let url;
+
+  try {
+    url = new URL(urlString);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+};
